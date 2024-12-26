16376e5661914e42fac34c13cd133a5d6441aeda
Dmitrii Kim <mitya_kim@icloud.com>
2024-12-25 21:32:51 -0800

    finish day 28

--- Day_28_Tkinter/main.py
+++ Day_28_Tkinter/main.py
@@ -2,21 +2,60 @@ from tkinter import *
 import time
 import math
 
-WORK_MIN = 25
-SHORT_BREAK_MIN = 5
-LONG_BREAK_MIN = 20
+WORK_MIN = 1
+SHORT_BREAK_MIN = 1
+LONG_BREAK_MIN = 1
+REPS = 0
+TIMER: None = None
 
 
 def start_timer():
-    count_down(300)
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    global  REPS
+    REPS += 1
+
+    if REPS % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Long Break", fg="green")
+    elif REPS % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Short Break", fg="lightgreen")
+    else:
+        count_down(work_sec)
+        title_label.config(text="Time to Work")
 
 def count_down(count):
+    global TIMER
     count_min = math.floor(count / 60)
     count_sec = count % 60
 
+    if count_sec == 0:
+        count_sec = "00"
+    elif count_sec < 10:
+        count_sec = f"0{count_sec}"
+
     canvas.itemconfig(timer_text, text=f'{count_min}:{count_sec}')
     if count > 0:
-        window.after(1000, count_down, count - 1)
+        TIMER = window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
+        mark = ''
+        for _ in range(math.floor(REPS/2)):
+            mark += '✔'
+        check_marks.config(text=mark)
+
+def reset_timer():
+    global TIMER
+    title_label.config(text='Timer')
+    window.after_cancel(TIMER)
+    canvas.itemconfig(timer_text, text='00:00')
+    check_marks.config(text='')
+    global REPS
+    REPS = 0
+
 
 window = Tk()
 window.title("Timer")
@@ -39,10 +78,10 @@ start_button.config(width=5, height=2, font=('Ariel', 15, 'bold'), command=start
 start_button.grid(column=0, row=2)
 
 reset_button = Button(text="Reset", highlightthickness=0, fg='blue')
-reset_button.config(width=5, height=2, font=('Ariel', 15, 'bold'))
+reset_button.config(width=5, height=2, font=('Ariel', 15, 'bold'), command=reset_timer)
 reset_button.grid(column=3, row=2)
 
-check_marks = Label(text='✔', fg='red', background='black', font=('Ariel', 30, 'bold'))
+check_marks = Label(fg='red', background='black', font=('Ariel', 30, 'bold'))
 check_marks.grid(column=1, row=2)
 
 
